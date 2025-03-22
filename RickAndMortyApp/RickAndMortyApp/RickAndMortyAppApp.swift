38c99485d4b78a371adb40e9ee42e260322a8b97
Carlos Gutiérrez Casado <carlosgutierrezcasado.1991@gmail.com>
2025-03-22 18:35:59 +0100

    First commit. Project created

---
+++ RickAndMortyApp/RickAndMortyApp/RickAndMortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyAppApp.swift
+//  RickAndMortyApp
+//
+//  Created by Carlos Gutiérrez Casado on 22/3/25.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RickAndMortyApp/RickAndMortyAppTests/RickAndMortyAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyAppTests.swift
+//  RickAndMortyAppTests
+//
+//  Created by Carlos Gutiérrez Casado on 22/3/25.
+//
+
+import Testing
+@testable import RickAndMortyApp
+
+struct RickAndMortyAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
